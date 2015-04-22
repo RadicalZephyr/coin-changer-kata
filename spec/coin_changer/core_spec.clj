ca8dc594c901c0f4c01d046730fb67c901724904
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-22 17:00:38 -0500

    Initial commit

---
+++ spec/coin_changer/core_spec.clj
@@ -0,0 +1,7 @@
+(ns coin-changer.core-spec
+  (:require [speclj.core :refer :all]
+            [coin-changer.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/coin_changer/core.clj
@@ -0,0 +1,1 @@
+(ns coin-changer.core)
